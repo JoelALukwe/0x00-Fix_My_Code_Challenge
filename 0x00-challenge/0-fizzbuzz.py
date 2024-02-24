9bf8e100c5bce8d7b2f8f32f28a462ef829c0315
JoelALukwe <alukwe7@gmail.com>
2024-02-24 14:13:54 -0800

    0. FizzBuzz

---
+++ 0x00-challenge/0-fizzbuzz.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" FizzBuzz """
+
+import sys
+
+def fizzbuzz(n);
+"""FizzBuzz function prints numbers starting from number 1 to n separated by a space.
+- For multiples of three print "Fizz" instead of the number and for multiples of five print "Buzz".
+- For numbers which are multiples of both three and five print "FizzBuzz"."""
+
+if n < 1;
+return
+
+tmp_result = []
+for i in range (1, n + 1);
+if (i % 3) == 0 and (i % 5) == 0: #Checks for both multiple of 3 and 5 first
+    tmp_result.append("FizzBuzz")
+elif (i % 3) == 0;
+tmp_result.append("Fizz")
+elif (i % 5) == 0;
+tmp_result.append("Buzz")
+else:
+    tmp_result.append(str(i))
+    print(" ".join(tmp_result))
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) <= 1:
+        print("Missing number")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
+        sys.exit(1)  # Exit the program with error status
+    number = int(sys.argv[1])  # Corrected assignment of number from command-line argument
+    fizzbuzz(number)  
