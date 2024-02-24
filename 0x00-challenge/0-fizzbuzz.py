1d691a8810891986b889705667d22969a8588d3b
JoelALukwe <alukwe7@gmail.com>
2024-02-24 14:15:26 -0800

    0. FizzBuzz

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -14,22 +14,22 @@ return
 tmp_result = []
 for i in range (1, n + 1);
 if (i % 3) == 0 and (i % 5) == 0: #Checks for both multiple of 3 and 5 first
-    tmp_result.append("FizzBuzz")
+tmp_result.append("FizzBuzz")
 elif (i % 3) == 0;
 tmp_result.append("Fizz")
 elif (i % 5) == 0;
 tmp_result.append("Buzz")
 else:
-    tmp_result.append(str(i))
-    print(" ".join(tmp_result))
+tmp_result.append(str(i))
+print(" ".join(tmp_result))
 
 
 
 if __name__ == '__main__':
-    if len(sys.argv) <= 1:
-        print("Missing number")
-        print("Usage: ./0-fizzbuzz.py <number>")
-        print("Example: ./0-fizzbuzz.py 89")
-        sys.exit(1)  # Exit the program with error status
-    number = int(sys.argv[1])  # Corrected assignment of number from command-line argument
-    fizzbuzz(number)  
+if len(sys.argv) <= 1:
+print("Missing number")
+print("Usage: ./0-fizzbuzz.py <number>")
+print("Example: ./0-fizzbuzz.py 89")
+sys.exit(1)  # Exit the program with error status
+number = int(sys.argv[1])  # Corrected assignment of number from command-line argument
+fizzbuzz(number)
